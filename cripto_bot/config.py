8e646f9da867f2d52fbc9639ff7b7f0091b67dc3
1337Artem <240368497+1337Artem@users.noreply.github.com>
2025-11-04 23:25:09 +0300

    Add files via upload

---
+++ cripto_bot/config.py
@@ -0,0 +1,13 @@
+TOKEN = ('8335618511:AAG2H6tgKF7C9rwZjztUNomSUiSyZaJUX5E')
+
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'франк': 'CHF',
+    'юань': 'CNY',
+    'иена': 'JPY',
+    'фунд': 'GBR'
+
+}
---
+++ cripto_bot/tellebott.py
@@ -0,0 +1,48 @@
+
+import telebot
+from config import keys, TOKEN
+from extensions import APIException, CryptoConverter
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message: telebot.types.Message):
+   text = f'Приветствую {message.from_user.first_name}!\nУзнать инструкцию: /help'
+   bot.reply_to(message, text)
+
+@bot.message_handler(commands=['help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите команду боту в следующнм формате:\n<имя валюты цену которой он хочет узнать> \
+    <имя валюты в которой надо узнать цену первой валюты> \
+    <количество первой валюты>\nУвидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.lower().split(' ')
+
+        if len(values) > 3:
+            raise APIException("Слишком много параметров.")
+        elif len(values) < 3:
+            raise APIException("Мало параметров.")
+
+        base, quote, amount = values
+        total_base = CryptoConverter.get_price(base, quote, amount)
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {base} в {quote} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+bot.polling(non_stop=True)
